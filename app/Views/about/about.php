9087eee44c80c85807c6bb4d16c8b60ee953304b
FahmiBahtiar <fahmibahtiar76@gmail.com>
2023-07-05 08:56:46 +0700

    For uas web

---
+++ app/Views/about/about.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script src="//cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/about.css'); ?>">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="home-section-navbar">
+        <div id="page-1" class="scroll-container scroll-page"></div>
+        <nav class="navbar">
+            <div class="nav-container">
+                <!-- LOGO -->
+                <div class="logo">GAME SUIT</div>
+                <!-- NAVIGATION MENU -->
+                <ul class="nav-links">
+                    <!-- NAVIGATION MENUS -->
+                    <div class="menu">
+                        <li><a href="../">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="https://sociabuzz.com/blimbing/tribe">Donation</a></li>
+                        <a href="/game"><button class="buttoncr"> Game Suit </button></a>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <!--Section 2: About me-->
+    <div class="background">
+        <div class="column-2">
+            <h1>About me</h1>
+            <p>It is an activity that has characteristics such as satire, intelligence, mobility which are found in the folk culture and played collectively in the times when the feature is limited and the possibility of entertainment is limited</p>
+            <br><br>
+
+            <p>Developer - Fahmi Bahtiar Adi Nugroho</p>
+        </div>
+        <!--Profile Card 3-->
+        <div class="col-md-5">
+            <div class="column-3 card profile-card-3">
+                <div class="background-block">
+                    <img src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile-sample1" class="background" />
+                </div>
+                <div class="profile-thumb-block">
+                    <img src="https://hydra.bot/_nuxt/icons/icon_512x512.0401fd.png" alt="profile-image" class="profile" />
+                </div>
+                <div class="card-content">
+                    <h2>Fahmi Bahtiar<small>Developer</small></h3>
+                        <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="column-3 large-profile"></div> -->
+    </div>
+</body>
+
+</html>
